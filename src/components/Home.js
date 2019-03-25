eb253a01687405d18af43fdbb30894bc900c810e
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2019-03-25 01:23:22 -0400

    update home text

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,52 +3,53 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div id="home-text">
         <p>
           We at Monday Home Improvement, LLC. Would like to thank you for
-          considering us for all of your home improvement needs. Our number one
-          priority is to provide our customers with the best customer service
-          and top notch work we can stand behind 100%. We are honest,
-          experienced, and will go out of our way to do a great job. Whether you
-          have $500 or $5,000 to spend on your home improvements, there are no
-          jobs too big or small! Give us a call today and let our experience go
-          to work for you.
+          considering us for your home improvement goals. Our number one
+          priority is to provide our customers with the highest notch work, work
+          we can stand behind 100%. We are honest, experienced, and experienced.
+          Whether you have $5,000 or $50,000 to spend on your home improvements,
+          there are no jobs too big or small! Give us a call today for a free
+          estimate.
         </p>
-
+        <center>
+          <p>We are licensed, Bonded, Insured, and OSHA-10 Certified.</p>
+        </center>
+        <br />
+        <br />
         <p>
-          Our company will help you achieve your goal, working with you
-          throughout the entire process of the project. We will also help you
-          select quality products and materials with the best prices on the
-          market. Thanks to our friends at Ferguson and Daltile, we are able to
-          provide you the largest selection of products for kitchen and
-          bathrooms.
+          We hope to work with you throughout the entire process of the project.
+          This could include helping you select quality products and materials
+          at the best prices on the market. Thanks to our friends at{" "}
+          <a href="https://www.ferguson.com/" target="_blank">
+            Ferguson
+          </a>{" "}
+          and{" "}
+          <a href="https://www.daltile.com/" target="_blank">
+            Daltile
+          </a>, we are able to provide you the largest selection of products for
+          kitchens and bathrooms.
         </p>
-        <p>Visit our photo gallery and get inspired!</p>
-        <p>Call today for a free estimate!</p>
-        <p>We are licensed, Bonded and Insured.</p>
-        <p>
-          Monday Home Improvement, LLC. is now an EPA Lead- Safe Certified Firm!
-        </p>
-        <p>
-          We are now certified by OSHA (Occupational Safety & Health
-          Administration)
-        </p>
-
-        <p>
-          Monday Home Improvement, LLC.<br />
-          Phone: (301) 399. 1094<br />
-          Fax: (301) 498. 5370 <br />
-          8830 Castlebury Court<br />
-          Laurel, MD 20723<br />
-          MondayHomeImprovement@hotmail.com<br />
-          <br />
-          <br />
-          www.DaltileProducts.com<br />
-          www.Ferguson.com<br />
-        </p>
+        <center>
+          <p>Visit our photo gallery and get inspired!</p>
+        </center>
       </div>
     );
   }
 }
 
 export default Home;
+
+// <p>
+//   Monday Home Improvement, LLC.<br />
+// Phone: (301) 399. 1094<br />
+// Fax: (301) 498. 5370 <br />
+// 8830 Castlebury Court<br />
+// Laurel, MD 20723<br />
+// MondayHomeImprovement@hotmail.com<br />
+// <br />
+// <br />
+// www.DaltileProducts.com<br />
+// www.Ferguson.com<br />
+// </p>
