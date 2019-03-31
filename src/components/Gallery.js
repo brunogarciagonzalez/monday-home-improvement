05c7df69a08baa43ae58883bb40a698e74af2b32
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2019-03-31 12:17:26 -0400

    include <Header />

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
-import { Tab } from "semantic-ui-react";
+import { Tab, Header } from "semantic-ui-react";
 import galleryPanes from "./../galleryPanes.js";
 
 class Gallery extends Component {
   render() {
     return (
       <div>
+        <Header as="h2" style={{ textAlign: "center" }}>
+          <span>Photo Gallery</span>
+        </Header>
+
         <Tab panes={galleryPanes} menu={{ secondary: true }} />
       </div>
     );
